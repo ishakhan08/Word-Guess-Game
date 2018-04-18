5ebd1f5f2e38af6a74d8437a7dc9304900978495
Isha Khan <khan.isha08@gmail.com>
2018-04-18 11:24:55 -0500

    Eh

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// window showing alphabets a to z 
+// showing blank word like - - - - -
+// make list of words like the name of bands created on Monday
+// pick a random word from list like in r p s
+// ask user to guess the word like in rps
+// if user guess matches with one of the word then it gets displayed
+// reduce the counter of no of guesses
+// if guess all the letters then user win
+// if counter becomes 0 then user looses like in rps
+// add a theme
+
+
