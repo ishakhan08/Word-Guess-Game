0efa1225ebc2f2a536fde9813afe7a0504b7c38c
Isha Khan <khan.isha08@gmail.com>
2018-04-19 23:42:50 -0500

    On it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,78 @@
-// window showing alphabets a to z 
-// showing blank word like - - - - -
-// make list of words like the name of bands created on Monday
-// pick a random word from list like in r p s
-// ask user to guess the word like in rps
-// if user guess matches with one of the word then it gets displayed
-// reduce the counter of no of guesses
-// if guess all the letters then user win
-// if counter becomes 0 then user looses like in rps
-// add a theme
+var wordlist = [
+    ["C", "H", "I", "C", "A", "G", "O"],
+      ["A","U","S","T","I","N"],
+      ["D","A","L","L","A","S"],
+      ["D","E","T","R","O","I","T"],
+      ["S","E","A","T","T","L","E"],
+      ["N","E","W","A","R","K"]
+    ]
+    var random = Math.floor((Math.random()*(wordlist.length-1))); 
+    
+    var newword = wordlist[random]; 
+    var randdomword = new Array(newword.length);
+    var initial = 0;
+    
+    
+    for (var i = 0; i < randdomword.length; i++){
+        randdomword[i] = "_ ";
+    }
+    
 
-
+    function printranddomword(){
+        for (var i = 0; i < randdomword.length; i++){
+        var demo = document.getElementById("demo");
+        var guessword = document.createTextNode(randdomword[i]);
+        demo.appendChild(guessword);
+        }
+    }
+    
+    
+    var enter = function(){
+        var f = document.wordform; 
+        var b = f.elements["wordtext"]; // the letter provided by the user
+        userGuess = userGuess.toUpperCase();
+        for (var i = 0; i < newword.length; i++){
+            if(newword[i] === userGuess){
+                randdomword[i] = userGuess + " ";
+                var answer = true;
+            }
+        b.value = "";
+        }
+        
+       
+        var demo = document.getElementById("demo");
+        demo.innerHTML=""; 
+        printranddomword();
+        
+       if(!answer){
+            var wrongletter = document.getElementById("wrongletter");
+            var guessword = document.createTextNode(" " + userGuess);
+            wrongletter.appendChild(guessword); 
+            initial++;
+            var hangman = document.getElementById("hangman");
+        hangman.src = "http://www.writteninpencil.de/Projekte/Hangman/hangman" + initial + ".png";
+        }
+        
+        var letterlist = true;
+        for (var i = 0; i < randdomword.length; i++){
+            if(randdomword[i] === "_ "){
+                letterlist = false;
+            }
+        }
+        if(letterlist){
+            window.alert("You win!");
+        }
+        
+        if(initial === 6){
+            window.alert("Uh...I guess you're dead now.");
+        }
+    }
+    
+    function init(){
+        printranddomword();
+    }
+    
+    window.onload = init;
+    
+    
+    
